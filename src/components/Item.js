31046d2c507df57da18ef73d924f55e42eeba9ed
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-06-03 22:28:25 -0700

    Adds an add to cart toggle

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [liState, setLiState] = useState("");
+  function handleAddToCart() {
+    if (!liState) {
+      setLiState("in-cart");
+    } else {
+      setLiState("");
+    }
+  }
+
   return (
-    <li className="">
+    <li className={liState}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddToCart}>
+        {liState ? "Remove from Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
